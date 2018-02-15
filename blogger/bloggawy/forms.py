434e77583aff320917713551866ae742f519b629
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-02-15 03:42:31 +0200

    add urls and test post

--- blogger/bloggawy/forms.py
+++ blogger/bloggawy/forms.py
@@ -2,12 +2,12 @@ from django import forms
 from .models import Student, Track
 
 
-class StudentForm(forms.ModelForm):
-    class Meta:
-        model = ModelName  # modify
-        fields = ('fieldname', 'fieldname', 'fieldname', 'fieldname')
-        widgets = {
-            'fieldname': forms.TextInput(attrs={'class': 'form-control'}),
-            'fieldname': forms.TextInput(attrs={'class': 'form-control'}),
-            'fieldname': forms.TextInput(attrs={'class': 'form-control'}),
-        }
+# class ModelName(forms.ModelForm):
+#     class Meta:
+#         model = ModelName  # modify
+#         fields = ('fieldname', 'fieldname', 'fieldname', 'fieldname')
+#         widgets = {
+#             'fieldname': forms.TextInput(attrs={'class': 'form-control'}),
+#             'fieldname': forms.TextInput(attrs={'class': 'form-control'}),
+#             'fieldname': forms.TextInput(attrs={'class': 'form-control'}),
+#         }
