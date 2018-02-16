cc9867baa9d89f764cf8f2d51384cc50b95820e0
Yasminabdallah <yasminabdalllahgaberm@gmail.com>
2018-02-17 01:16:40 +0200

    updated adminpanel page

--- blogger/bloggawy/views.py
+++ blogger/bloggawy/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from  .forms import SignUpForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 
@@ -29,7 +30,10 @@ def registeration(request):
     return render(request, 'web/registration.html',{"form": form})
 
 def adminpanel(request):
-    return(request,'admin/adminpanel.html')
+    users=User.objects.all()
+    context={"allusers":users}
+    
+    return render(request,'admin/adminpaneltwo.html',context)
 
       
     	
